c4393ccc9d5710817b63e6797c476aa11b2c414e
demirsubasic2001 <subasicd2000@gmail.com>
2023-03-30 21:51:41 +0200

    shopping-cart-added

--- inlation_calculator/apps.js
+++ inlation_calculator/apps.js
@@ -1,30 +1,91 @@
-function inflationCalculator() {
-    let inflationRate = document.querySelector("#inflationRate");
-    let money = document.querySelector("#money");
-   
-    inflationRate = parseFloat(inflationRate.value);
-    money = parseFloat(money.value);
+//varijabla koja ce sabrati cene svega sto smo stavili u korpu
+let allTotal = 0;
 
 
-   let years = document.querySelector("#years").value;
-   years = parseFloat(years);
 
 
-   //Formula za inflaciju
-  let worth = money + (money * (inflationRate / 100));
+//Funckija za dodavanje elemenata
+addToCart = (element) => {
+    //Selektovanje varijabli
+    let mainEl = element.closest(".single-item");
+    let price = mainEl.querySelector('.price').textContent;
+    let name = mainEl.querySelector("h3").textContent;
+    let quantity = mainEl.querySelector("input").value;
+    let cartItems = document.querySelector(".cart-items");
 
 
-  for(let i = 1; i < years; i++) {
-    worth += worth * (inflationRate / 100);
-  }
+    //Uslov da ne moze dodati 0 kolicinu
+    if(parseInt(quantity) > 0) {
+       
+        //da uklonimo znak $ sa price
+        price = price.substring(1);
+
+
+        //total je jednak mnozenju price i quantity koje smo pretvorili u number
+        let total = parseInt(price) * parseInt(quantity);
+       
+        //Da se na all total dodaje i stara vrednost na novu
+        allTotal += total;
+
+
+        //Dodaje stvari koje smo odabrali i selektuje one koje ce ispisati
+        cartItems.innerHTML += `<div class = "cart-single-item">
+                                   <h3>${name}</h3>
+                                   <p>$${price} x ${quantity} = $<span>${total}</span></p>
+                                   <button onclick="removeFromCart(this)" class = "remove-item">Ukloni</button>
+                                </div>`;
  
-   worth = worth.toFixed(2);
+        //Selektovali smo all total i sta ce ispisati u njega
+        document.querySelector(".total").textContent = `Total: $${allTotal}`
 
 
+        //Da se tekst dugmeta promeni nakon klika
+        element.textConent = "Dodato";
 
 
-   let newElement = document.createElement('div');
-   newElement.className = 'new-value';
-   newElement.innerText = `Danasnjih ${money}€ vrijedi isto kao ${worth}€ za ${years} godina`;
-   document.querySelector('.container').appendChild(newElement);
-} 
+        //Disable-amo dugme posle pritiska
+        element.setAttribute("disabled", "true");
+    } else {
+        alert("Odaberi kolicinu")
+    }
+}
+
+
+//Funkcija koja ce pritiskom na button ukloniti ono sto smo pre dodali
+removeFromCart = (element) => {
+    //Selektovanje varijabli
+   let mainEl = element.closest('.cart-single-item');
+   let price = mainEl.querySelector("p span").textContent;
+   let name = mainEl.querySelector("h3").textContent;
+   let products = document.querySelectorAll(".single-item");
+
+
+    price = parseInt(price);
+
+
+    //Oduzimamo od all total price
+    allTotal -= price;
+
+
+    //Ispisujemo u all total onu cifru koju smo dobili
+    document.querySelector(".total").textContent = `Total: $${allTotal}`;
+   
+    //Uklanjamo ceo red onoga sto smo dodali
+    mainEl.remove();
+   
+
+
+    //Pravimo petlju da prodjemo kroz svaki clan
+    products.forEach(function(product) {
+        let itemName = product.querySelector('.si-content h3').textContent;
+      if(itemName === name) {
+        //Vracamo vrednost na nulu
+        product.querySelector(".actions input").value = 0;
+        //Deaktiviramo disabled
+        product.querySelector(".actions button").removeAttribute('disabled');
+        //Vracamo da opet na dugme pise "dodaj"
+        product.querySelector(".actions button").textContent = "Dodaj";
+      }
+    })
+}
+
