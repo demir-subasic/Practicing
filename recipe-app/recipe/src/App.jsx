c8e88d414b7349c25d2354aa90f490714d29b35e
Demir Subasic <subasicd2000@gmail.com>
2023-09-21 19:04:36 +0200

    finished styling

--- recipe-app/recipe/src/App.jsx
+++ recipe-app/recipe/src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import hamburger from './assets/hamburger.png'
+import { Header, AppNameComponent, AppIcon, SearchComponent, SearchInput } from './components/headerComponent';
+import {RecipeContainer, CoverImage, RecipeName,IngredientsText, SeeMoretext} from './components/recipeComponents';
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const RecipeListContainer = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding: 30px;
+justify-content: space-evenly;
+`;
+
+
+
 
 function App() {
 
   return (
-    <p>Recipe project</p>
+      <Container>
+      <Header>
+        <AppNameComponent><AppIcon src={hamburger} alt="" /></AppNameComponent>
+        <SearchComponent>
+          <SearchInput placeholder='Search Recipe' type="text" />
+        </SearchComponent>
+      </Header>
+      <RecipeListContainer>
+      <RecipeContainer>
+        <CoverImage src={hamburger}/>
+        <RecipeName>Jelo</RecipeName>
+        <IngredientsText>prvi button</IngredientsText>
+        <SeeMoretext>recept</SeeMoretext>
+      </RecipeContainer>
+      </RecipeListContainer>
+    </Container>
   )
 }
 
