5183497098f0dfeee691286de7c5fbc6ab95a3a8
Demir Subasic <subasicd2000@gmail.com>
2023-09-22 12:15:12 +0200

    some changes with api

--- recipe-app/recipe/src/App.jsx
+++ recipe-app/recipe/src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Axios from 'axios';
 import hamburger from './assets/hamburger.png'
 import { Header, AppNameComponent, AppIcon, SearchComponent, SearchInput } from './components/headerComponent';
 import {RecipeContainer, CoverImage, RecipeName,IngredientsText, SeeMoretext} from './components/recipeComponents';
+import axios from 'axios';
+
+
+const APP_ID = "a392a591";
+const APP_KEY = "80e28a425e5eaa996f19f49c145b8d99";
+
+
 const Container = styled.div`
 display: flex;
 flex-direction: column;
@@ -21,12 +29,28 @@ justify-content: space-evenly;
 
 function App() {
 
+  const [timeoutId, updateTimeoutId] = useState();
+
+
+  const fetchRecipe = (searchString) => {
+   axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`
+   ).then(function (response) {
+    console.log(response);
+   })
+  }
+
+  const onTextChange = (e) => {
+    clearTimeout();
+   const timeout = setTimeout(() => fetchRecipe(e.target.value), 500);
+   updateTimeoutId(timeout)
+  }
+
   return (
       <Container>
       <Header>
         <AppNameComponent><AppIcon src={hamburger} alt="" /></AppNameComponent>
         <SearchComponent>
-          <SearchInput placeholder='Search Recipe' type="text" />
+          <SearchInput placeholder='Search Recipe' onChange={onTextChange}/>
         </SearchComponent>
       </Header>
       <RecipeListContainer>
