3346e32755b75945a16f7c220a55d39e481cb3ae
Demir Subasic <subasicd2000@gmail.com>
2023-09-29 11:25:22 +0200

    logic for walls i project and movement

--- advanced maze game/script.js
+++ advanced maze game/script.js
@@ -178,7 +178,111 @@ const Maze = (Width, Height) => {
                     y: 0
                 };
             break;
+            case 2: 
+                startCoord = {
+                    x: height - 1,
+                    y: 0
+                };
+                endCoord = {
+                    x: 0,
+                    y: width - 1
+                }
+            break;
+            case 3: 
+                startCoord = {
+                    x: height - 1,
+                    y: width - 1
+                };
+                endCoord = {
+                    x: 0,
+                    y: 0
+                };
+            break;
         }
+    }
+
+    genMap();
+    defineStartEnd();
+    defineMaze();
+
+}
+
+const DrawMaze = (Maze, ctx, cellSize, endSprite = null) => {
+    var map = Maze.map();
+    var cellSize = cellSize;
+    var drawEndMethod;
+    ctx.lineWidth = cellSize = cellSize / 40;
+
+
+    this.redrawMaze = function(size) {
+        cellSize = size;
+        ctx.lineWidth = cellSize / 50;
+        drawMap();
+        drawEndMethod();
+    };
+
+    const drawCell = (xCord, yCord, cell) => {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + cellSize, y);
+        ctx.stroke();
+    if (cell.s === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + cellSize);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke();
+    }
+    if (cell.e === false) {
+        ctx.beginPath();
+        ctx.moveTo(x + cellSize, y);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke;
+    }
+    if (cell.w === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + cellSize);
+        ctx.stroke();
+    }
+}
+    const drawMap = () => {
+       for(x = 0; x < map.length; x++){
+        for (y = 0; y < map[x].length; y++){
+            drawCell(x, y, map[x][y]);
+        }
+       }
+    }
+
+    const drawEndFlag = () => {
+        var coord = Maze.endCoord();
+        var gridSize = 4;
+        var fraction = cellSize / gridSize - 2;
+        var colorSwap = true;
+        for (let y = 0; y < gridSize; y++){
+            if(gridSize % 2 == 0){
+                colorSwap = !colorSwap;
+            }
+            for (let x = 0; x < gridSize; x++){
+                ctx.beginPath();
+                ctx.rect{
+                    coord.x * cellSize + x * fraction + 4.5,
+                    coord.y * cellSize + y * fraction + 4.5,
+                    fraction,
+                    fraction
+                };
+                if (colorSwap) {
+                    ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+                } else {
+                    ctx.fillStyle = "rgba(255, 255, 255, 0.8)"
+                }
+                ctx.fill();
+                colorSwap = !colorSwap;
+            }
+        }
+    }
+
+    const drawEndSprite = () => {
+        
     }
 
 }
