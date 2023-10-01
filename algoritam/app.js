f56dcbd815fdc920c498ea296835d5bc5f46f89b
Demir Subasic <subasicd2000@gmail.com>
2023-10-01 15:21:33 +0200

    completed several tasks in the field of algorithms

--- algoritam/app.js
+++ algoritam/app.js
@@ -1,5 +1,4 @@
-// 1--- Output the largest number from each string  
-// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// 1--- Output the largest number from each array
 
 function largestOfFour(arr) {
     const result = [];
@@ -9,3 +8,43 @@ function largestOfFour(arr) {
     }
     return result;
 }
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
+
+
+// 2--- Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+    return str.slice(str.length - target.length) === target;
+}  
+console.log(confirmEnding("Bastian", "n"));
+
+
+
+// 3--- Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+    let string = "";
+    for (let i = 0; i < num; i++){
+      string += str;
+    }
+    return string;
+  }
+  
+ console.log( repeatStringNumTimes("*", 3));
+
+
+
+
+// 4--- Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+    if (str.length > num){
+      return str.slice(0, num) + '...'
+    } else {
+      return str;
+    }
+}
+
+console.log(truncateString("This-is-some text about programming", 8))
