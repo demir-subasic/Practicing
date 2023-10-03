30bb9fb235858515e4593d9270867d9be14da776
Demir Subasic <subasicd2000@gmail.com>
2023-10-03 18:05:58 +0200

    start to learning node.js basics

---
+++ backend/node/global.js
@@ -0,0 +1,23 @@
+// const amount = 12;
+
+// if (amount < 10) {
+//     console.log('number is small');
+// } else {
+//     console.log('large number')
+// }
+
+// console.log(`hey its my first node app`)
+
+
+
+// GLOBALS
+
+// console.log(__dirname) // path to current directory 
+// console.log(__filename) // file name
+// reuqire - function to use modules 
+// module - info about currennt module(file)
+// process - info about where the program is being executed
+
+// setInterval(() => {
+//     console.log('interval je postavljen')
+// }, 1000)
