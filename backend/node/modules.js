1dc1ac92896d56576fcd22767974c0d9e08ae667
Demir Subasic <subasicd2000@gmail.com>
2023-10-04 17:51:37 +0200

    alternative way to exports modules

--- backend/node/modules.js
+++ backend/node/modules.js
@@ -3,6 +3,9 @@ console.log(names)
 
 const sayHi = require('./modulesUtils')
 
+const data = require('./alternativeFlavor')
+console.log(data)
+
 sayHi('susan')
 sayHi(names.peter)
 sayHi(names.john)
