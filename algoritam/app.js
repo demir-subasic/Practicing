741708d75daf0aa9962dd65dadaaeee4b8b6f15d
Demir Subasic <subasicd2000@gmail.com>
2023-10-05 21:01:43 +0200

    exercise with splice, slice method

--- algoritam/app.js
+++ algoritam/app.js
@@ -108,4 +108,17 @@ console.log(titleCase("I'm a little tea pot"));
 
 
 
+// 9 --- You are given two arrays and an index. Copy each element of the first array into the second array, in order.Begin inserting elements at index n of the second array.Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+
+  let arr = arr2.slice();
+ 
+  arr.splice(n,0,...arr1)
+  return arr;
+ }
+ 
+ frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
 
