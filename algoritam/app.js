3d4ce1a19b0c4447e6c965280ac40b81d2bb8d29
Demir Subasic <subasicd2000@gmail.com>
2023-10-07 11:47:31 +0200

    algorith task

--- algoritam/app.js
+++ algoritam/app.js
@@ -170,3 +170,16 @@ function mutation(arr) {
 
 console.log(mutation(["hello", "hey"]))
 
+
+
+// 13 --- Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += size){
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
