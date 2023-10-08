91198decc51cfed3c5e7357095ae71fe846ddc71
Demir Subasic <subasicd2000@gmail.com>
2023-10-08 20:36:22 +0200

    basic modules in node.js finished

--- backend/node/modules.js
+++ backend/node/modules.js
@@ -4,10 +4,11 @@ console.log(names)
 const sayHi = require('./modulesUtils')
 
 const data = require('./alternativeFlavor')
-console.log(data)
 
-sayHi('susan')
-sayHi(names.peter)
-sayHi(names.john)
+require('./mindGrenade')
+
+// sayHi('susan')
+// sayHi(names.peter)
+// sayHi(names.john)
 
 
