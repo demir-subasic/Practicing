4ad3e00ab608c54e2ebc1060571ee3a7b5702dae
Demir Subasic <subasicd2000@gmail.com>
2023-10-10 17:35:53 +0200

    some testing with async method in node.js

--- backend/node/fsSync.js
+++ backend/node/fsSync.js
@@ -1,5 +1,5 @@
 const {readFileSync, writeFileSync} = require('fs')
-
+console.log('start')
 
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
@@ -8,3 +8,6 @@ writeFileSync('./content/resultSync.text',
  `Here is the resultt : ${first}, ${second}`, 
  {flag: 'a'}
 )
+
+console.log('done with task')
+console.log('starting  the next one')
