dfff82b19e7f2b12b99c0d1acc669fc7c9f94f39
Demir Subasic <subasicd2000@gmail.com>
2023-10-13 19:04:50 +0200

    work with Stream in node.js and finished fundamental things about that

---
+++ backend/node/readStream.js
@@ -0,0 +1,15 @@
+// const {writeFileSync} = require('fs')
+
+// for (let i = 0; i < 1000; i++){
+//     writeFileSync('./content/big.txt', `hello world ${i}\n`, {flag:'a'})
+// }
+
+const {createReadStream} = require('fs')
+
+const stream = createReadStream('./content/big.txt', {highWaterMark: 90000, encoding: 'utf-8'})
+
+stream.on('data', (result) => {
+console.log(result)
+})
+
+stream.on('error', (err) => console.log(err))
