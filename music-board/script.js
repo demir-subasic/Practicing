8c7c394b9897c62c41e629af9bc2f90727a302b4
demir-subasic <subasicd2000@gmail.com>
2024-01-06 23:49:01 +0100

    this is new version of music board added with real link and fixed some problems

--- music-board/script.js
+++ music-board/script.js
@@ -1,15 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const boxes = document.querySelectorAll('.box');
+const soundBoxes = document.querySelectorAll('.sound-box');
 
-    boxes.forEach(box => {
-        box.addEventListener('click', function () {
-            const soundId = box.getAttribute('data-sound');
-            const sound = document.getElementById(soundId);
-
-            if (sound) {
-                sound.currentTime = 0;
-                sound.play();
-            }
-        });
-    });
+soundBoxes.forEach(box => {
+  box.addEventListener('click', () => {
+    const soundUrl = box.dataset.soundUrl;
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${soundUrl}?enablejsapi=1&origin=${window.location.origin}`;
+    iframe.style.display = 'none';
+    iframe.onload = () => {
+      const player = new YT.Player(iframe);
+      player.addEventListener('onReady', () => {
+        player.playVideo();
+      });
+    };
+    document.body.appendChild(iframe);
+  });
 });
+
+// Load the YouTube Player API script
+const tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+document.body.appendChild(tag);
